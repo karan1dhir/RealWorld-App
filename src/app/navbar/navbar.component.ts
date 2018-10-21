4f57a7418cbb1750c2f8c3a152b9b547aec3309b
SoloWarrior <karandhir64@yahoo.co.in>
2018-10-21 08:49:04 +0530

    working on publish articles

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,24 +1,16 @@
 import { Component, OnInit } from "@angular/core";
 import { JwtService } from "../services/jwt.service";
-import { Router } from "@angular/router";
-import { SignInComponent } from "../sign-in/sign-in.component";
 
 @Component({
   selector: "app-navbar",
   templateUrl: "./navbar.component.html",
-
   styleUrls: ["./navbar.component.css"]
 })
 export class NavbarComponent implements OnInit {
-  isAuth: boolean;
-  constructor(private jwtservice: JwtService, private router: Router) {}
+  constructor(private jwtservice: JwtService) {}
 
   ngOnInit() {}
-  logout() {
-    console.log("You are logged out");
-    this.jwtservice.destroyToken();
-    this.router.navigateByUrl("/");
-  }
+
   checkSignIn() {
     if (this.jwtservice.getToken()) return true;
     else return false;
--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { JwtService } from "../services/jwt.service";
+import { Router } from "@angular/router";
 @Component({
-  selector: 'app-settings',
-  templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.css']
+  selector: "app-settings",
+  templateUrl: "./settings.component.html",
+  styleUrls: ["./settings.component.css"]
 })
 export class SettingsComponent implements OnInit {
+  constructor(private jwtservice: JwtService, private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  logout() {
+    console.log("You are logged out");
+    this.jwtservice.destroyToken();
+    this.router.navigateByUrl("/");
   }
-
 }
